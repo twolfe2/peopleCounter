7e8426beacad3ca9a5a80b0282ed47b09a56c2b7
Thomas Wolfe <tewolfe2@gmail.com>
2016-09-26 17:22:00 -0700

    app ready to build

--- App/Containers/Counter.js
+++ App/Containers/Counter.js
@@ -10,12 +10,26 @@ import Increment from '../Components/Increment';
 import Decrement from '../Components/Decrement';
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      count: 0
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container} >
-        <Text style={styles.text}>Hello World</Text>
-        <Increment />
-        <Decrement />
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.text}>Welcome to People Counter!</Text>
+          <Text>Counting people since 2016</Text>
+        </View>
+        <View style={styles.counter}>
+          <Increment />
+          <Text>{this.state.count}</Text>
+          <Decrement />
+        </View>
       </View>
     );
   }
@@ -24,14 +38,23 @@ class Counter extends Component {
 const styles = StyleSheet.create({
   container: {
       flex: 1,
-      justifyContent: 'center',
+      justifyContent: 'flex-start',
+      marginTop: 25,
       alignItems: 'center',
-      backgroundColor: '#F5FCFF',
+      backgroundColor: '#F5FCFF', 
+    },
+    header: {
+      alignItems:'center',
     },
     text: {
       fontSize: 20,
       textAlign: 'center',
       margin: 10,
+    },
+    counter: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1
     }
 });
 
